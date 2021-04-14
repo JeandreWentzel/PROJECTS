ee3808308ac4eb29c0c80c512d0380abaa0f0d79
JeandreWentzel <75695126+JeandreWentzel@users.noreply.github.com>
2021-04-14 18:50:03 +0200

    Update TCP Client.py

--- TCP Client.py
+++ TCP Client.py
@@ -1,17 +1,13 @@
-#!/usr/bin/env python
-# -*- coding:utf8 -*-
-# auther; 18793
-# Date：2019/9/6 10:44
-# filename: TCP_Client.py
+#J WENTZEL
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 8888))  # 建立连接
+s.connect(('127.0.0.1', 8888)) 
 
-# 打印接收到欢迎消息
+
 print(s.recv(1024).decode('utf-8'))
 
-# 客户端程序发送人名数据给服务端
+
 list_name = ["a", "b", "c", "d", "e", "Michael", "Tracy", "Sarah"]
 for data in list_name:
     s.send(data.encode())
